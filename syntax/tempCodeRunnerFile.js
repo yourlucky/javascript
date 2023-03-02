aaa2aa9777b2e64c57f655763c602c72504b9a03
yourlucky <yoonchae.na@gmail.com>
2023-03-01 21:03:42 -0600

    simple login

--- syntax/tempCodeRunnerFile.js
+++ syntax/tempCodeRunnerFile.js
@@ -1,16 +1,12 @@
-// var letter2 =
-//   "Dear " +
-//   name +
-//   "\n\nLorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
-//   name +
-//   " Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa egoing qui officia deserunt mollit anim id est laborum. " +
-//   name;
+console.log("1" + "1");
+console.log(1 + 1);
 
-// // Template literals
-// var letter = `Dear ${name}
+console.log("1" - "1");
+console.log(1 - 1);
 
-// Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ${name} Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ${
-//   1 + 1
-// } Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa egoing qui officia deserunt mollit anim id est laborum. ${name}`;
+var str =
+  "hahahdanpa;sncv;zxncvklnkdldnasklclasjdfpojq  lkanslkdvfnsadlkfnsalkfdnaskldfn";
 
-// console.log(letter);
+str.length;
+
+console.log("             a          ".length);
